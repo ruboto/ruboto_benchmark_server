0966bddce4d175b5e5d83edaaf888f91e725eeff
Uwe Kubosch <uwe@kubosch.no>
2011-08-30 21:50:28 +0200

    * Prepared to receive data...

--- db/migrate/20110830154406_create_startups.rb
+++ db/migrate/20110830154406_create_startups.rb
@@ -1,14 +1,14 @@
 class CreateStartups < ActiveRecord::Migration
   def self.up
     create_table :startups do |t|
-      t.integer :startup_time, :null => false
-      t.string :package, :null => false
-      t.string :package_version, :null => false
-      t.string :manufacturer, :null => false
-      t.string :device, :null => false
-      t.string :android_version, :null => false
+      t.integer :startup_time,           :null => false
+      t.string :package,                 :null => false
+      t.string :package_version,         :null => false
+      t.string :manufacturer,            :null => false
+      t.string :device,                  :null => false
+      t.string :android_version,         :null => false
       t.string :ruboto_platform_version, :null => false
-      t.string :ruboto_app_version, :null => false
+      t.string :ruboto_app_version,      :null => false
 
       t.timestamps
     end
