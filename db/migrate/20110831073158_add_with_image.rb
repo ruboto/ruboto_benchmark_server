62bf24ecba327d992c8f6da389346ba3550f54ed
Uwe Kubosch <uwe@kubosch.no>
2011-08-31 09:39:10 +0200

    * Added field with_image

--- db/migrate/20110831073158_add_with_image.rb
+++ db/migrate/20110831073158_add_with_image.rb
@@ -2,7 +2,7 @@ class AddWithImage < ActiveRecord::Migration
   def self.up
     add_column :startups, :with_image, :boolean
     execute 'UPDATE startups SET with_image = true'
-    change_column :startups, :with_image, :null => false
+    change_column :startups, :with_image, :boolean, :null => false
   end
 
   def self.down
