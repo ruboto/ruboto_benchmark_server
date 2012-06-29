ea40a92c3684579d9d6433c3bd64a5e53bb5604f
Uwe Kubosch <uwe@kubosch.no>
2012-06-30 01:46:43 +0200

    * Improved Top Ten display

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -4,7 +4,7 @@ class MeasurementsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @measurements }
+      format.xml { render :xml => @measurements }
     end
   end
 
@@ -13,7 +13,7 @@ class MeasurementsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @measurement }
+      format.xml { render :xml => @measurement }
     end
   end
 
@@ -22,7 +22,7 @@ class MeasurementsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @measurement }
+      format.xml { render :xml => @measurement }
     end
   end
 
@@ -40,10 +40,10 @@ class MeasurementsController < ApplicationController
     respond_to do |format|
       if @measurement.save
         format.html { redirect_to(@measurement, :notice => 'Measurement was successfully created.') }
-        format.xml  { render :xml => @measurement, :status => :created, :location => @measurement }
+        format.xml { render :xml => @measurement, :status => :created, :location => @measurement }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @measurement.errors, :status => :unprocessable_entity }
+        format.xml { render :xml => @measurement.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -54,10 +54,10 @@ class MeasurementsController < ApplicationController
     respond_to do |format|
       if @measurement.update_attributes(params[:measurement])
         format.html { redirect_to(@measurement, :notice => 'Measurement was successfully updated.') }
-        format.xml  { head :ok }
+        format.xml { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @measurement.errors, :status => :unprocessable_entity }
+        format.xml { render :xml => @measurement.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -68,15 +68,23 @@ class MeasurementsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to(measurements_url) }
-      format.xml  { head :ok }
+      format.xml { head :ok }
     end
   end
 
   def top_ten
-    @groups = Measurement.all.group_by{|s| [s.manufacturer, s.model, s.android_version, s.ruboto_app_version, s.ruboto_platform_version, s.package_version]}
-    @rows = @groups.map do |group, measurements|
-      [measurements.sort_by(&:duration)[(measurements.size / 4).to_i].duration] + group
-    end.sort_by{|r| r[0]}
+    measurements_by_test = Measurement.all.group_by { |m| m.test }
+    @benchmarks = measurements_by_test.map do |test, measurements_for_test|
+      measurements_by_params = measurements_for_test.group_by { |s| [s.manufacturer, s.model, s.android_version, s.ruboto_app_version, s.ruboto_platform_version, s.package, s.package_version] }
+      rows = measurements_by_params.map do |params, measurements_for_params|
+        {
+            :median => measurements_for_params.sort_by(&:duration)[(measurements_for_params.size / 4).to_i].duration,
+            :params => params,
+            :count => measurements_for_params.size,
+        }
+      end.sort_by { |r| r[:median] }
+      {:name => test, :rows => rows, :count => measurements_for_test.size}
+    end.sort_by { |r| [-r[:count], -r[:rows].size] }
   end
 
 end
