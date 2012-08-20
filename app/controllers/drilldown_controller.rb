5452148a8f05595a55645cfe60626f8d0d66259c
Uwe Kubosch <uwe@kubosch.no>
2012-08-20 21:35:29 +0200

    * Fixed the totals of the fastest times

--- app/controllers/drilldown_controller.rb
+++ app/controllers/drilldown_controller.rb
@@ -135,7 +135,7 @@ class DrilldownController < ApplicationController
     {
         :value => values[-1],
         :count => total_count,
-        :volume => result_rows.map{|r| r[:volume]}.min,
+        :volume => result_rows.map{|r| r[:volume] > 0 ? r[:volume] : nil}.compact.min,
         :volume_compensated => result_rows.inject(0){ |t, r| t + r[:volume_compensated] * r[:count]} / total_count,
         :row_count => result_rows.inject(0) { |t, r| t + r[:row_count] },
         :nodes => result_rows.inject(0) { |t, r| t + r[:nodes] } + 1,
